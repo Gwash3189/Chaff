55a010777942075d21a7391065d9912715965ed4
Adam Beck <adamb3189@gmail.com>
2014-03-21 19:57:32 +1100

    Add ITestable interface

--- Chaff.ts
+++ Chaff.ts
@@ -1,26 +1,27 @@
 module Chaff {
-    class Mock<T>{
-        private CreatedType: T;
-        constructor(private Type: any){}
-
-        public With(mutator: (obj:T)=>void): Mock<T>{
-            var t = new this.Type();
-            this.CreatedType = t;
-            mutator(t);
-            return this;
-        }
-        public Private(mutator: (obj: any)=>void): Mock<T>{
-            if(!this.CreatedType){
-                var t = new this.Type();
-                this.CreatedType = t;
-            }
-            mutator(t);
-            return this;
-        }
-
-        public Create(): T{
-            return this.CreatedType
-        }
-    }
+    export interface ITestable<SpecClass,Subject>{
+		Mock: Mocker.Mock<Subject>;
+	}
+	export class Mock<T>{	
+		private CreatedType: T;
+		constructor(private Type: any){}
+		public With(mutator: (obj:T)=>void): Mock<T>{
+			var t = new this.Type();
+			this.CreatedType = t;
+			mutator(t);
+			return this;
+		}
+		public Private(mutator: (obj: any)=>void): Mock<T>{
+			if(!this.CreatedType){
+				var t = new this.Type();
+				this.CreatedType = t;
+			}
+			mutator(t);
+			return this;
+		}
+		public Create(): T{
+			return this.CreatedType
+		}
+	}	
 }
 
